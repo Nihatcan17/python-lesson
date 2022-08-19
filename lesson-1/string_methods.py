38e98238be894b2604b11a313bbb4be501905b4f
nihat <nihatesatcan17@gmail.com>
2022-08-19 16:56:35 +0300

    9. bölümde kaldım

---
+++ lesson-1/string_methods.py
@@ -0,0 +1,13 @@
+name = "nick"
+
+#print(len(name))
+
+#print (name.upper())
+
+#print(name.isalpha())
+
+#print(name.count("i"))
+
+#print(name.replace("i","c"))
+
+#print(name*3)
---
+++ lesson-1/type-cast.py
@@ -0,0 +1,11 @@
+from turtle import st
+
+
+x = 1
+y = 2.0
+z= "3"
+
+print("X is "+str(x))
+print("Y is "+str(y))
+print(z*3)
+
