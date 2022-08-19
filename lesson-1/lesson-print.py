f2d303ec520a87a021373969d743bced956b21ad
nihat <nihatesatcan17@gmail.com>
2022-08-19 15:12:44 +0300

    new lessons added

---
+++ lesson-1/lesson-print.py
@@ -0,0 +1,20 @@
+first_name = "bro"
+last_name = "code"
+full_name = first_name + " " +  last_name
+
+
+print ("Hello " + " " + full_name )
+print (type(name))
+
+
+age = 28
+age +=  1
+print ("your age is: " +str(age))
+
+height = 186
+print("Your height is: " +str(height) + "cm")
+
+human = True
+print(human)
+print(type(human))
+print("are you a human: " +str(human))
---
+++ lesson-1/multiple_assignment.py
@@ -0,0 +1,13 @@
+#name = "nick"
+#age = 28
+#attractive = True
+
+name, age, attractive = "nick", 28, True
+
+print(name)
+print(age)
+print(attractive)
+
+nick = patrick = Sandy = squidward = 30
+
+print(patrick)
